42913a1c9bc49d9d75fd6f584bfedf02ec7b0d5a
Holger Wahlen <Tarquin@web.de>
2020-11-20 15:44:42 +0100

    Added implementation, extended tests.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,53 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+; ------------------------------------------------------
+; index of a character in the alphabet:
+; a -> 0, b -> 1 etc.
+(defn char-to-index [c]
+  (- (int c) (int \a))
+)
 
-(defn decode [keyword message]
-  "decodeme")
+; ------------------------------------------------------
+; shift character c by i positions in the alphabet
+; (modulo 26)
+(defn shift-character [c i]
+  (char (+ (mod (+ (char-to-index c) i (char-to-index \a)) 26) (int \a)))
+)
 
-(defn decipher [cipher message]
-  "decypherme")
+; ------------------------------------------------------
+; get n-th element, modulo collection length
+(defn nth-mod-count [coll index]
+  (nth coll (mod index (count coll)))
+)
 
+; ------------------------------------------------------
+; encode cleartext message using the given keyword
+(defn encode [keyword cleartext]
+  (apply str (for [i (range 0 (count cleartext))]
+    (shift-character
+      (nth cleartext i)
+      (char-to-index (nth-mod-count keyword i))
+    )
+)))
+
+; ------------------------------------------------------
+; decode cipher using the given keyword
+(defn decode [keyword cipher]
+  (apply str (for [i (range 0 (count cipher))]
+    (shift-character
+      (nth cipher i)
+      (- (char-to-index (nth-mod-count keyword i)))
+    )
+)))
+
+; ------------------------------------------------------
+; get the keyword from cipher and cleartext
+(defn decipher [cipher cleartext]
+  (let [candidate (decode cleartext cipher)]
+    (loop [k 1]
+      (if (= cipher (encode (subs candidate 0 k) cleartext))
+        (subs candidate 0 k)
+        (recur (inc k))
+      )
+    )
+))
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,6 +2,55 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest char-to-index-test
+  (is (= (char-to-index \a) 0))
+  (is (= (char-to-index \j) 9))
+)
+
+(deftest shift-character-test
+  (is (= (shift-character \a 0) \a))
+  (is (= (shift-character \a 10) \k))
+  (is (= (shift-character \a 30) \e))
+)
+
+(deftest nth-mod-count-test
+  (is (= (nth-mod-count "string" 2) \r))
+  (is (= (nth-mod-count "string" 10) \n))
+)
+
+(deftest encode-test
+  (is (=
+    "hmkbxebpxpmyllyrxiiqtoltfgzzv"
+    (encode "vigilance" "meetmeontuesdayeveningatseven")))
+  (is (=
+    "egsgqwtahuiljgs"
+    (encode "scones" "meetmebythetree")))
+)
+
+(deftest decode-test
+  (is (=
+    "meetmeontuesdayeveningatseven"
+    (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
+  (is (=
+    "meetmebythetree"
+    (decode "scones" "egsgqwtahuiljgs")))
+)
+
+(deftest decipher-test
+  (is (=
+    "vigilance"
+    (decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")))
+  (is (=
+    "scones"
+    (decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "packmyboxwithfivedozenliquorjugs")))
+  (is (=
+    "abcabcx"
+    (decipher "hfnlphoontutufa" "hellofromrussia")))
+)
+
+; ---------------------------------------------------------------
+; tests provided by gigasquid
+
 (deftest test-encode
   (testing "can encode a message with a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"

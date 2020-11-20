3ae977eb2da954b77d9d058759827cd696025bfb
Holger Wahlen <Tarquin@web.de>
2020-11-20 16:00:43 +0100

    Added pseudo-implementation.

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,23 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [[:fox :goose :corn :you] [:boat] []])
 
 (defn river-crossing-plan []
-  start-pos)
+  [
+    start-pos
+    [[:fox :corn]        [:boat :goose :you] []]
+    [[:fox :corn]        [:boat]             [:goose :you]]
+    [[:fox :corn]        [:boat :you]        [:goose]]
+    [[:fox :corn :you]   [:boat]             [:goose]]
+    [[:corn]             [:boat :fox :you]   [:goose]]
+    [[:corn]             [:boat]             [:goose :fox :you]]
+    [[:corn]             [:boat :goose :you] [:fox]]
+    [[:corn :goose :you] [:boat]             [:fox]]
+    [[:goose]            [:boat :corn :you]  [:fox]]
+    [[:goose]            [:boat]             [:fox :corn :you]]
+    [[:goose]            [:boat :you]        [:fox :corn]]
+    [[:goose :you]       [:boat]             [:fox :corn]]
+    [[]                  [:boat :goose :you] [:fox :corn]]
+    [[]                  [:boat]             [:fox :corn :goose :you]]
+  ]
+)

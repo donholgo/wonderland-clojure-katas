cdbe5209aec0ef4ea1c34732e38e9c99fa46858f
Holger Wahlen <Tarquin@web.de>
2020-11-20 15:39:36 +0100

    Added implementation, extended tests.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,41 @@
 (ns wonderland-number.finder)
 
+; =============================================
+; number-to-sorted-vector
+
+(defn number-to-sorted-vector [num]
+  (sort (clojure.string/split (str num) #""))
+)
+
+
+; =============================================
+; same-digits?
+
+(defn same-digits? [num1 num2]
+  (= (number-to-sorted-vector num1) (number-to-sorted-vector num2))
+)
+
+
+; =============================================
+; possible-wonderland-number?
+
+(defn possible-wonderland-number? [num]
+  (and
+    (= (count (str num)) 6)
+    (same-digits? num (* num 2))
+    (same-digits? num (* num 3))
+    (same-digits? num (* num 4))
+    (same-digits? num (* num 5))
+    (same-digits? num (* num 6))
+  )
+)
+
+
+; =============================================
+; wonderland-number
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter possible-wonderland-number? (range 100000 1000000)))
+)
+
+; → 142857
--- wonderland-number/test/wonderland_number/finder_test.clj
+++ wonderland-number/test/wonderland_number/finder_test.clj
@@ -2,6 +2,31 @@
   (:require [clojure.test :refer :all]
             [wonderland-number.finder :refer :all]))
 
+(deftest number-to-sorted-vector-test
+  (is (= (number-to-sorted-vector 123) '("1" "2" "3")))
+  (is (= (number-to-sorted-vector 221133) '("1" "1" "2" "2" "3" "3")))
+)
+
+(deftest same-digits-test
+  (is (= (same-digits? 3 3) true))
+  (is (= (same-digits? 5 6) false))
+  (is (= (same-digits? 123 123) true))
+  (is (= (same-digits? 123 456) false))
+  (is (= (same-digits? 123 321) true))
+  (is (= (same-digits? 123 1223) false))
+)
+
+(deftest possible-wonderland-number?-test
+  (is (= (possible-wonderland-number? 42) false))
+  (is (= (possible-wonderland-number? 100000) false))
+  (is (= (possible-wonderland-number? 142857) true))
+  (is (= (possible-wonderland-number? 1234567) false))
+)
+
+
+; ---------------------------------------------
+; tests provided by gigasquid
+
 (defn hasAllTheSameDigits? [n1 n2]
   (let [s1 (set (str n1))
         s2 (set (str n2))]

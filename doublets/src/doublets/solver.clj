f0fbb3ca183327de27520faa2705976371336e42
Holger Wahlen <Tarquin@web.de>
2020-11-20 15:33:48 +0100

    Added implementation, extended tests.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,60 @@
                (slurp)
                (read-string)))
 
+
+; ================================================================
+; are word1 and word2 of equal length
+; and different by exactly one letter?
+(defn one-letter-diff?
+  [word1 word2]
+  (and
+    (= (count word1) (count word2))
+    (= (count (filter true? (map #(not= %1 %2) word1 word2))) 1)
+  )
+)
+
+; ================================================================
+; does vector _vect_ contain element _element_?
+(defn vector-contains? [vect element]
+  (boolean (some #(= element %) vect))
+)
+
+; ================================================================
+; for a vector _doublet_, find all words _w_ in the
+; dictionary that
+;  a) differ from the last word in _doublet_ by one letter
+;  b) are not contained in _doublet_
+; and return a list that for each such word _w_
+; contains the conjunction of _doublet_ and _w_ 
+; (see examples in tests)
+(defn add-possible-continuations [doublet]
+  (->> words
+    (filter
+      #(and
+        (one-letter-diff? (last doublet) %)
+        (not (vector-contains? doublet %))
+      )
+    )
+    (map #(conj doublet %))
+  )
+)
+
+; ================================================================
+; starting at _word1_, build tree of possible doublets until one
+; has _word2_ at the end
+(defn search [word1 word2]
+  (let [continue? #(not= (last %) word2)]
+    (tree-seq continue? add-possible-continuations [word1])
+  )
+)
+
+; ================================================================
+; from all the doublets starting at _word1_ that "search" creates,
+; get the first one that has _word2_ at the end
 (defn doublets [word1 word2]
-  "make me work")
+  (->> (search word1 word2)
+    (filter #(= (last %) word2))
+    (first)
+    (vec)
+  )
+)
--- doublets/test/doublets/solver_test.clj
+++ doublets/test/doublets/solver_test.clj
@@ -2,6 +2,75 @@
   (:require [clojure.test :refer :all]
             [doublets.solver :refer :all]))
 
+
+(deftest one-letter-diff?-test
+  (is (true? (one-letter-diff? "cat" "hat")))
+  (is (false? (one-letter-diff? "cat" "cats")))
+  (is (false? (one-letter-diff? "one" "two")))
+)
+
+(deftest vector-contains?-test
+  (is (true? (vector-contains? [1 2 3] 2)))
+  (is (false? (vector-contains? [1 2 3] 4)))
+)
+
+(deftest add-possible-continuations-test
+  (is (=
+    (add-possible-continuations ["look"])
+    '(["look" "book"] ["look" "lock"] ["look" "loon"] ["look" "loot"] ["look" "cook"])
+))
+  (is (=
+    (add-possible-continuations ["look" "book"])
+    '(["look" "book" "boor"] ["look" "book" "bonk"] ["look" "book" "cook"])
+))
+)
+
+(deftest search-test
+  (is (=
+    (search "wheat" "cheap")
+    '(["wheat"] ["wheat" "cheat"] ["wheat" "cheat" "cheap"])
+  ))
+  (is (=
+    (search "door" "look")
+    '(["door"] ["door" "boor"] ["door" "boor" "book"] ["door" "boor" "book" "look"] ["door" "boor" "book" "bonk"] ["door" "boor" "book" "bonk" "bank"] ["door" "boor" "book" "cook"] ["door" "boor" "book" "cook" "look"])
+  ))
+)
+
+(deftest doublets-test
+  (is (=
+    ["head" "heal" "teal" "tell" "tall" "tail"]
+    (doublets "head" "tail")
+  ))
+  (is (=
+    (doublets "door" "lock")
+    ["door" "boor" "book" "look" "lock"]
+  ))
+  (is (=
+    (doublets "bank" "loan")
+    ["bank" "bonk" "book" "look" "loon" "loan"]
+  ))
+  (is (=
+    (doublets "wheat" "bread")
+    ["wheat" "cheat" "cheap" "cheep" "creep" "creed" "breed" "bread"]
+  ))
+  (is (=
+    (doublets "door" "tall")
+    []
+  ))
+  (is (=
+    []
+    (doublets "ye" "freezer")
+  ))
+)
+
+
+
+
+
+
+; ---------------------------------------------------------------
+; tests provided by gigasquid
+
 (deftest solver-test
   (testing "with word links found"
     (is (= ["head" "heal" "teal" "tell" "tall" "tail"]

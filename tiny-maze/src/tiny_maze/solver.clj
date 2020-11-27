0dd6c246199ad37ca620e0fc42d9415db455bc78
Holger Wahlen <Tarquin@web.de>
2020-11-27 17:24:35 +0100

    Added tiny maze solution (inspired by manuel-colmenero).

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,56 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+; inspired by manuel-colmenero :-)
+; https://github.com/manuel-colmenero/wonderland-clojure-katas/blob/master/tiny-maze/src/tiny_maze/solver.clj
+
+
+(defn start-position
+  "Finds the start position in the maze."
+  [maze]
+  (first (for [x (range (count maze))
+              y (range (count (first maze)))
+              :when (= :S (get-in maze [x y]))]
+            [x y])
+  )
+)
+
+
+(defn valid-target-neighbors
+  "List of all neighbors of (x, y) in the maze that contain '0' or ':E'."
+  [maze [x y]]
+  (filter #(#{0 :E} (get-in maze %)) (list [(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]))
+)
+
+; broken down version:
+; (defn neighbors [[x y]]
+;   "List with the neighbors (existing or not) of position (x, y)."
+;   (list [(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)])
+; )
+; (defn valid-target-position?
+;   "True if a step to position (x, y) is allowed, i.e. if the
+;   position is in the maze and contains '0' or ':E'."
+;   [maze [x y]]
+;   (boolean (#{0 :E} (get-in maze [x y])))
+; )
+; (defn valid-target-neighbors
+;   "List of all neighbors of (x, y) in the maze that contain '0' or ':E'."
+;   [maze [x y]]
+;   (filter #(valid-target-position? maze %) (neighbors [x y]))
+; )
+
+
+(defn walk-recursive [maze [x y]]
+  (let [next-maze (assoc-in maze [x y] :x)]
+    (if (= (get-in maze [x y]) :E)
+      next-maze
+      (some #(walk-recursive next-maze %)
+            (valid-target-neighbors maze [x y])
+            )
+    )
+  )
+)
+
+(defn solve-maze [maze]
+  (walk-recursive maze (start-position maze))
+)
+
--- tiny-maze/test/tiny_maze/solver_test.clj
+++ tiny-maze/test/tiny_maze/solver_test.clj
@@ -2,6 +2,67 @@
   (:require [clojure.test :refer :all]
             [tiny-maze.solver :refer :all]))
 
+(def maze3
+  [[:S 0 1]
+   [1  0 1]
+   [1  0 :E]])
+
+(def maze3a
+  [[:x 0 1]
+   [1  0 1]
+   [1  0 :E]])
+
+(def maze4
+  [[:E 0 0 0]
+   [1  1 0 1]
+   [0  0 0 0]
+   [:S 0 1 0]])
+
+(def maze5
+  [[0  0 :S  0  0]
+   [1  0  1  0  1]
+   [0  0  1  0  0]
+   [0  1  0  1  0]
+   [0  0 :E  0  0]])
+
+(deftest start-position-test
+  (is (= (start-position maze3) [0 0]))
+  (is (= (start-position maze4) [3 0]))
+  (is (= (start-position maze5) [0 2]))
+)
+
+(deftest valid-target-neighbors-test
+  (is (= (valid-target-neighbors maze3 [1 0]) '([1 1])))
+  (is (= (valid-target-neighbors maze3 [1 1]) '([0 1] [2 1])))
+  (is (= (valid-target-neighbors maze3 [2 1]) '([1 1] [2 2])))
+)
+
+
+; (deftest neighbors-test
+;   (is (= (neighbors [3 7]) [[2 7] [4 7] [3 6] [3 8]]))
+; )
+; (deftest valid-target-position?-test
+;   (is (false? (valid-target-position? maze3 [0 0])))
+;   (is (true? (valid-target-position? maze3 [0 1])))
+;   (is (false? (valid-target-position? maze3 [0 2])))
+;   (is (true? (valid-target-position? maze3 [2 2])))
+;   (is (false? (valid-target-position? maze3a [0 0])))
+; )
+
+
+(deftest solve-maze-test
+  (is (= (solve-maze maze3)) [[:x :x 1] [1 :x 1] [1 :x :x]])
+  (is (= (solve-maze maze4)) [[:x :x :x 0] [1 1 :x 1] [:x :x :x 0] [:x 0 1 0]])
+  (is (or
+    (= (solve-maze maze5) [[0 :x :x 0 0] [1 :x 1 0 1] [:x :x 1 0 0] [:x 1 0 1 0] [:x :x :x 0 0]])
+    (= (solve-maze maze5) [[0 0 :x :x 0] [1 0 1 :x 1] [0 0 1 :x :x] [0 1 0 1 :x] [0 0 :x :x :x]])
+  ))
+)
+
+
+; ---------------------------------------------------------
+; provided by gigasquid
+
 (deftest test-solve-maze
   (testing "can find way to exit with 3x3 maze"
     (let [maze [[:S 0 1]

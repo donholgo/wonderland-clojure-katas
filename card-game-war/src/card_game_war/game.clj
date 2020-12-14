81418af5cd432ec724344aa48b3700bde0538417
Holger Wahlen <Tarquin@web.de>
2020-12-14 17:38:26 +0100

    Added card game solution/tests.

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,64 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn rank-index [rank]
+  (.indexOf ranks rank)
+)
 
-(defn play-game [player1-cards player2-cards])
+(defn suit-index [suit]
+  (.indexOf suits suit)
+)
+
+(defn first-card-better? [card1 card2]
+  "true if card1 is better than card2, otherwise false."
+  (let [suit1 (first card1)
+        suit1-index (suit-index suit1)
+        rank1 (second card1)
+        rank1-index (rank-index rank1)
+        suit2 (first card2)
+        suit2-index (suit-index suit2)
+        rank2 (second card2)
+        rank2-index (rank-index rank2)]
+    (or (> rank1-index rank2-index)
+        (and (= rank1-index rank2-index)
+             (> suit1-index suit2-index)))
+    )
+)
+
+
+
+
+(defn play-round [player1-card player2-card]
+  "Determine the winner of a round with the two played cards."
+  (if (first-card-better? player1-card player2-card)
+    :player1
+    :player2
+    )
+  )
+
+(defn play-game [player1-cards player2-cards]
+  (cond (empty? player1-cards) :player2-wins
+        (empty? player2-cards) :player1-wins
+        :else (let [[card1 & rest1] player1-cards
+                    [card2 & rest2] player2-cards]
+                (if (= :player1 (play-round card1 card2))
+                  (recur (conj rest1 card1 card2) rest2)
+                  (recur rest1 (conj rest2 card1 card2))
+                  )
+                )
+        )
+  )
+
+
+(defn run-game [cards]
+  (let [shuffled-cards (split-at (/ (count cards) 2) (shuffle cards))
+        player-1-cards (first shuffled-cards)
+        player-2-cards (last shuffled-cards)]
+    (if (= :player1-wins (play-game player-1-cards player-2-cards))
+      (println "Player 1 wins.")
+      (println "Player 2 wins.")
+      )
+    )
+  )
+
+(run-game cards)
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -3,13 +3,45 @@
             [card-game-war.game :refer :all]))
 
 
+(deftest rank-index-test
+  (is (< (rank-index 2) (rank-index 3)))
+  (is (< (rank-index 4) (rank-index 7)))
+  (is (< (rank-index 9) (rank-index 10)))
+  (is (< (rank-index 10) (rank-index :jack)))
+  (is (< (rank-index :jack) (rank-index :queen)))
+  (is (< (rank-index :queen) (rank-index :king)))
+  (is (< (rank-index :king) (rank-index :ace)))
+)
+
+(deftest suit-index-test
+  (is (< (suit-index :spade) (suit-index :club)))
+  (is (< (suit-index :club) (suit-index :diamond)))
+  (is (< (suit-index :diamond) (suit-index :heart)))
+)
+
+(deftest first-card-better?-test
+  (is (false? (first-card-better? [:spade 3] [:spade 8])))
+  (is (true? (first-card-better? [:heart :queen] [:heart 10])))
+  (is (false? (first-card-better? [:club :jack] [:diamond :jack])))
+)
+
+(deftest play-round-test
+  (is (= (play-round [:spade 10] [:heart 5]) :player1))
+  (is (= (play-round [:diamond 3] [:club 8]) :player2))
+)
+
+(deftest play-game-test
+  (is (= :player1-wins (play-game [[:heart 8] [:diamond :king] [:spade :jack]] [[:diamond 2] [:spade 4] [:club 5]])))
+  (is (= :player2-wins (play-game [[:diamond 2] [:spade 4] [:club 5]] [[:heart 8] [:diamond :king] [:spade :jack]])))
+)
+
+
+
 ;; fill in  tests for your game
-(deftest test-play-round
-  (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings"))
+;(deftest test-play-round
+;  (testing "the highest rank wins the cards in the round"
+;    (is (= 0 1)))
+;)
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
